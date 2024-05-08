a4174dc604e2eb0d1e943eb6991a93d057cc07e0
Sebastian Evers <sebastian.evers154@gmail.com>
2024-05-08 00:56:36 -0300

    Se sube proyecto con spring boot por 1ra vez

---
+++ Back/Disfraces/src/test/java/Grupo2/Disfraces/DisfracesApplicationTests.java
@@ -0,0 +1,13 @@
+package Grupo2.Disfraces;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DisfracesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

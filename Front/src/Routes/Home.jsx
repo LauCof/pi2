3919b4cd18016f5496a87efbb458fe8dab4df148
LuisPenia <luis.luispena@gmail.com>
2024-05-11 02:24:39 -0400

    "User Story #2"

--- Front/src/Routes/Home.jsx
+++ Front/src/Routes/Home.jsx
@@ -1,12 +1,36 @@
 import Card from "../Components/Card/Card";
 import "./Home.css";
+import lupa from "../assets/lupa.png";
+import { Link } from 'react-router-dom';
+
 
 const Home = () => {
   return (
     <div className="home">
-      <Card />
-      <Card />
-      <Card />
+
+      <section className="buscador">
+          <input type="text" placeholder="¿Qué Disfraz buscas?"/>
+          <Link><img src={lupa} alt="" /></Link>
+      </section>
+      
+      <section>
+        <h2>SECCION</h2>
+        <div>
+          <Card />
+          <Card />
+          <Card />
+        </div>  
+      </section>
+ 
+      <section className="recomendado">
+        <h2>RECOMENDACIONES</h2>
+        <div>
+          <Card />
+          <Card />
+          <Card />
+        </div>        
+      </section>
+  
     </div>
   );
 };
